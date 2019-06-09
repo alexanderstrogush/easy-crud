027f5f7b093e729c7d2943a8fabd2633171d2481
Alexander Strogush <strogushalex@gmail.com>
2019-06-09 23:32:18 +0300

    UserDao interface added

--- src/main/java/com/alexstrog/dao/implementation/CarDaoImpl.java
+++ src/main/java/com/alexstrog/dao/implementation/CarDaoImpl.java
@@ -1,7 +1,8 @@
 package com.alexstrog.dao.implementation;
 
 import com.alexstrog.dao.AbstractDao;
+import com.alexstrog.dao.CarDao;
 import com.alexstrog.model.Car;
 
-public class CarDaoImpl extends AbstractDao<Car, Long> {
+public class CarDaoImpl extends AbstractDao<Car, Long> implements CarDao {
 }
